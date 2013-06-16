ac2f0a009b7331c5f8791f9fd92042eb7a05d620
Harry van Haaren <harryhaaren@gmail.com>
2013-06-16 23:50:16 +0100

    -Updated ports definitions, updated UI

--- dsp/ports.h
+++ dsp/ports.h
@@ -19,29 +19,36 @@ typedef enum {
   
   MASTER_VOL,
   
-  OSC_1_MOD,
-  OSC_1_VOL,
+  COMP_ATTACK,
+  COMP_DECAY,
+  COMP_THRES,
+  COMP_RATIO,
+  COMP_MAKEUP,
   
-  OSC_2_MOD,
-  OSC_2_VOL,
+  // p*2... is to place hold that index for the functionality before.
+  // Actual interaction with the value is by comparison of "port" variable
+  // reading:
+  //    if ( port > PAD_GAIN && port < PAD_GAIN + 16 )
+  // writing:
+  //    write( int(PAD_GAIN) + padNumber, &value );
   
-  OSC_3_MOD,
-  OSC_3_VOL,
+  PAD_GAIN,
+  pg2,pg3,pg4,pg5,pg6,pg7,pg8,pg9,pg10,pg11,pg12,pg13,pg14,pg15,pg16,
   
-  LFO_MOD_AMP,
-  LFO_MOD_SPEED,
+  PAD_ATTACK,
+  pa2,pa3,pa4,pa5,pa6,pa7,pa8,pa9,pa10,pa11,pa12,pa13,pa14,pa15,pa16,
   
-  LFO_FREQ_AMP,
-  LFO_FREQ_SPEED,
+  PAD_DECAY,
+  pd2,pd3,pd4,pd5,pd6,pd7,pd8,pd9,pd10,pd11,pd12,pd13,pd14,pd15,pd16,
   
-  FILTER_FREQ,
-  FILTER_Q,
-  FILTER_TYPE,
+  PAD_SUSTAIN,
+  ps2,ps3,ps4,ps5,ps6,ps7,ps8,ps9,ps10,ps11,ps12,ps13,ps14,ps15,ps16,
   
-  REVERB_SIZE,
-  REVERB_DAMPING,
-  REVERB_WET,
-  REVERB_ENABLE,
+  PAD_RELEASE,
+  pr2,pr3,pr4,pr5,pr6,pr7,pr8,pr9,pr10,pr11,pr12,pr13,pr14,pr15,pr16,
+  
+  PAD_PAN,
+  pp2,pp3,pp4,pp5,pp6,pp7,pp8,pp9,pp10,pp11,pp12,pp13,pp14,pp15,pp16,
   
 } PortIndex;
 
